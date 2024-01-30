59a44d52cae44302ce77a8a9da5532fbf157cc74
kash1ma <ayollo.matvey@gmail.com>
2024-01-31 02:00:59 +0300

    step 3 done

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from "readline-sync";
+
+export function getUserName() {
+  // Use the `question` function to prompt the user for input
+  const name = readlineSync.question("What is your name? ");
+
+  return name;
+}
+
+const userName = getUserName();
+console.log("Hello, " + userName + "!");
